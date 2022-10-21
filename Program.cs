8b03df9889e3a1f5f6c9cc12ea92f47a6e05f628
IvanGalenko <98175502+IvanGalenko@users.noreply.github.com>
2022-10-21 17:55:12 +0300

    Добавлен блок кода с разными типами данных (3.3.2)

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
+            /* string MyName;
             MyName = "Ivan";
             Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Привет мир");
+            Console.WriteLine("\t Мне 23 года");
+            Console.WriteLine("\t My name is \n  Ivan");
+            Console.WriteLine('\u0040');
+            Console.WriteLine('\x23'); */
+
+            string MyName = "Ivan";
+            byte MyAge = 23;
+            bool HaveIApet = true;
+            double MyShoeSize = 41.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+
         }
     }
 }
