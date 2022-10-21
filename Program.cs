1880efc7a9c0491de8724c7b8daa72b89cf7464a
IvanGalenko <98175502+IvanGalenko@users.noreply.github.com>
2022-10-21 18:32:37 +0300

    Добавлен блок кода для задания 3.6.7

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FA
+﻿using System;
+
+namespace FA
 {
     internal class Program
     {
@@ -12,7 +14,7 @@
             Console.WriteLine("\t Мне 23 года");
             Console.WriteLine("\t My name is \n  Ivan");
             Console.WriteLine('\u0040');
-            Console.WriteLine('\x23'); */
+            Console.WriteLine('\x23'); 
 
             string MyName = "Ivan";
             byte MyAge = 23;
@@ -22,9 +24,32 @@
             Console.WriteLine("My name is " + MyName);
             Console.WriteLine("MyAge " + MyAge);
             Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
+            Console.WriteLine("My shoe size is " + MyShoeSize); 
 
+            DaysOfWeek MyFavoriteDay;
+
+            MyFavoriteDay = DaysOfWeek.Friday;
+
+            Console.WriteLine(MyFavoriteDay); */
+
+            double result = 10 % 3;
+            Console.WriteLine(result);
 
         }
+        /*
+        enum DaysOfWeek : byte
+        {
+            Tuesday,
+            Monday,
+            Wednesday,
+            Friday
+        }
+
+        enum Semaphore
+        {
+            Red = 100, 
+            Yellow = 200, 
+            Green = 300
+        } */
     }
 }
