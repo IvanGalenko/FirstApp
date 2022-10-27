1114a41de092f8c189705b9f38d17c36f21e922e
IvanGalenko <98175502+IvanGalenko@users.noreply.github.com>
2022-10-27 20:08:42 +0300

    Добавлены изменения (4.1.18)

--- Program.cs
+++ Program.cs
@@ -62,7 +62,7 @@ namespace FA
             Console.WriteLine(C); 
             var inv = true;
             var result = !inv;
-            Console.WriteLine(result); */
+            Console.WriteLine(result);
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
             var color = Console.ReadLine();
@@ -88,6 +88,41 @@ namespace FA
                 Console.ForegroundColor = ConsoleColor.Black;
 
                 Console.WriteLine("Your color is cyan!");
+            } */
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
             }
         }
         #region Модуль 1.3
