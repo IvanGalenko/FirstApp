ef439b04288ede19feb3a36c4906bbe94841a556
IvanGalenko <98175502+IvanGalenko@users.noreply.github.com>
2022-11-03 18:27:05 +0300

    Добавлен пункт 4.3.9

--- Program.cs
+++ Program.cs
@@ -233,12 +233,18 @@ namespace FA
 
             Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
 
-            Console.ReadKey(); */
+            Console.ReadKey(); 
             Console.WriteLine("Введите своё имя");
             var name = Console.ReadLine();
             for (int i = name.Length - 1; i >= 0; i--)
             {
                 Console.Write(name[i] + " ");
+            } */
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+            Console.WriteLine(array[0, 0]);
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
             }
         }
         #region Модуль 1.3
