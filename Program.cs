5d94da2420b74ac62fa8e79b9875eaad3656de7c
IvanGalenko <98175502+IvanGalenko@users.noreply.github.com>
2022-11-04 11:44:36 +0300

    Добавлен пункт 4.3.17

--- Program.cs
+++ Program.cs
@@ -317,6 +317,28 @@ namespace FA
             }
             Console.WriteLine(pol);
             */
+            //4.3.17
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            int temp;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    {
+                        if (arr[i,j] > arr[i,k])
+                        {
+                            temp = arr[i,k];
+                            arr[i,k] = arr[i,j];
+                            arr[i,j] = temp;
+                        }
+                    }
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+            }
         }
         #region Модуль 1.3
         /*
