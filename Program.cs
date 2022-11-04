a3cc0db77b6f8d33372c284acb8f9eacdc44c652
IvanGalenko <98175502+IvanGalenko@users.noreply.github.com>
2022-11-04 10:23:03 +0300

    Добавлен пункт 4.3.14

--- Program.cs
+++ Program.cs
@@ -274,7 +274,7 @@ namespace FA
             foreach (var item in arr)
             {
                 Console.Write(item);
-            } */
+            } 
             //4.3.13
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
             int sum = 0;
@@ -283,6 +283,22 @@ namespace FA
                 sum += arr[i];
             }
             Console.WriteLine(sum);
+            //4.3.14
+            int[][] array = new int[3][];
+
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+            foreach (var num in array)
+            {
+                foreach (var item in num)
+                {
+                    Console.Write(item + " ");
+                }
+            } */
+            
+
         }
         #region Модуль 1.3
         /*
